07b22a08382c719de884c37880c5d3c89f540502
Antorkarmokar28 <antorkarmokar28@gmail.com>
2025-12-03 21:12:05 +0600

    Fetching dynamic category data and show in ui

--- script/index.js
+++ script/index.js
@@ -1,11 +1,22 @@
 function loadCategories() {
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then((res) => res.json())
-    .then((data) => displayCategories(data));
+    .then((data) => displayCategories(data.categories));
 }
 
 function displayCategories(categories) {
-  console.log(categories);
+  const categoryContainer = document.getElementById("category-container");
+  for (let i = 0; i < categories.length; i++) {
+    // get single category
+    const category = categories[i];
+    console.log(category);
+    const containerDiv = document.createElement("div");
+    containerDiv.innerHTML = `
+    <button class="btn hover:bg-[#FF1F3D] hover:text-white hover:font-bold">${category.category}</button>
+    `;
+    // add button on the categoryContainer
+    categoryContainer.appendChild(containerDiv);
+  }
 }
 
 loadCategories();
